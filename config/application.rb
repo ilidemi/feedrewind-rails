40eada1c5640296cbe5f12707753ca3f52ddea1b
Ilia Demianenko <ilia.demianenko@gmail.com>
2021-10-08 17:47:55 -0700

    No autoload lib

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module RssCatchupRails
     Delayed::Worker.sleep_delay = 0.1
     Delayed::Worker.raise_signal_exceptions = :term
 
-    config.autoload_paths += Dir["#{config.root}lib/**/"]
-    config.eager_load_paths += Dir["#{config.root}lib/**/"]
+    Rails.autoloaders.main.ignore(Rails.root.join('app/lib'))
   end
 end
