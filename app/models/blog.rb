9a6e5d3a67f0e0b420eae5ed2f29aabe0135e774
Ilia Demianenko <ilia.demianenko@gmail.com>
2021-10-25 14:28:18 -0700

    Random id for blogs

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,12 +2,25 @@ class Blog < ApplicationRecord
   default_scope { where(discarded_at: nil) }
   scope :discarded, -> { where.not(discarded_at: nil) }
 
+  before_create :generate_random_id
+
   belongs_to :user, optional: true
   has_many :posts, dependent: :destroy
   has_many :schedules, dependent: :destroy
   has_one :current_rss, dependent: :destroy
   validates :name, presence: true
   validates :url, presence: true
+
+  def generate_random_id
+    new_id = generate_random_bigint
+    while Blog.exists?(new_id)
+      new_id = generate_random_bigint
+    end
+
+    # Race condition may happen if two instances generate the same id at the same time, which is highly
+    # unlikely
+    self.id = new_id
+  end
 
   def discarded?
     self.discarded_at.present?
@@ -36,5 +49,11 @@ class Blog < ApplicationRecord
     raise ActiveRecord::RecordNotDestroyed.new unless discarded?
 
     method(:destroy).super_method.call
+  end
+
+  private
+
+  def generate_random_bigint
+    SecureRandom.random_bytes(8).unpack("q").first & ((1 << 63) - 1)
   end
 end
