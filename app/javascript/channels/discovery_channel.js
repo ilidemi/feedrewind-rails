b4b3e28325f5a3021b5f723c4bf21b6bfbbafb14
Ilia Demianenko <ilia.demianenko@gmail.com>
2022-03-16 19:48:37 -0700

    Handle looks_wrong with javascript Monitor websocket staleness

--- app/javascript/channels/discovery_channel.js
+++ app/javascript/channels/discovery_channel.js
@@ -1,19 +1,24 @@
 import consumer from "./consumer"
 
 window.discoverySubscribe = (blogId, dataHandler) => {
-    return consumer.subscriptions.create({channel: "DiscoveryChannel", blog_id: blogId}, {
-        connected() {
-            // Called when the subscription is ready for use on the server
-        },
+    const subscription = consumer.subscriptions.create(
+        {channel: "DiscoveryChannel", blog_id: blogId},
+        {
+            connected() {
+                // Called when the subscription is ready for use on the server
+            },
 
-        disconnected() {
-            // Called when the subscription has been terminated by the server
-        },
+            disconnected() {
+                // Called when the subscription has been terminated by the server
+            },
 
-        received(data) {
-            dataHandler(data);
+            received(data) {
+                dataHandler(data);
+            }
         }
-    });
+    );
+
+    return [subscription, consumer.connection.monitor];
 };
 
 window.discoveryUnsubscribe = (subscription) => {
