886240b0b0b7796e4dd0fe9c6ea5eccf5bf35135
Ilia Demianenko <ilia.demianenko@gmail.com>
2022-04-05 12:30:41 -0700

    Prettier schedule layout

--- analysis/crawling/db.rb
+++ analysis/crawling/db.rb
@@ -1,7 +1,7 @@
 require 'pg'
 
 def connect_db
-  PG.connect(host: "172.25.86.5", dbname: 'rss_catchup_analysis', user: "postgres")
+  PG.connect(host: "172.20.54.108", dbname: 'rss_catchup_analysis', user: "postgres")
 end
 
 def unescape_bytea(bytea)
