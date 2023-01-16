b457a71f1e02f5fd02fe55573944ee83c6795e08
Ilia Demianenko <ilia.demianenko@gmail.com>
2023-01-16 13:17:37 -0800

    db ip

--- analysis/crawling/db.rb
+++ analysis/crawling/db.rb
@@ -1,7 +1,7 @@
 require 'pg'
 
 def connect_db
-  PG.connect(host: "172.25.143.198", dbname: 'rss_catchup_analysis', user: "postgres")
+  PG.connect(host: "172.22.122.102", dbname: 'rss_catchup_analysis', user: "postgres")
 end
 
 def unescape_bytea(bytea)
